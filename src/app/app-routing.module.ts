8dfdb2a73d7c6238664a64eac62aee45dcc76021
Theo wald <theo83wald@gmail.com>
2021-05-26 14:31:21 +0200

    empezamos formularios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {
+    path:'template',
+    loadChildren: () => import('./template/template-routing.module').then(m => m.TemplateRoutingModule)
+  },
+  {
+    path:'reactive',
+    loadChildren: () => import('./reactive/reactive-routing.module').then(m => m.ReactiveRoutingModule)
+  },
+  {
+    path: '**',
+    redirectTo: 'template'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
