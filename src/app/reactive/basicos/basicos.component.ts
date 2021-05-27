5de5e357e872d8ca113ca0e709f068b23c304ad8
Theo wald <theo83wald@gmail.com>
2021-05-27 16:54:33 +0200

    fin 27_05_2021

--- src/app/reactive/basicos/basicos.component.ts
+++ src/app/reactive/basicos/basicos.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-basicos',
@@ -6,11 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class BasicosComponent implements OnInit {
+export class BasicosComponent {
 
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  // miFormulario: FormGroup = new FormGroup({
+  //   'nombre'     : new FormControl('Orange'),
+  //   'precio'     : new FormControl(3),
+  //   'existencias': new FormControl(24)
+  // });
+
+
+  miFormulario: FormGroup = this.fb.group({
+    nombre: ['Birne'],
+    precio: [ 0 ],
+    existencias: [ 0 ]
+  })
+
+
+  constructor( private fb: FormBuilder) { }
 
 }
