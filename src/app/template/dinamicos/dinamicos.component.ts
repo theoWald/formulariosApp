c795b5a16a47dad1d14d0447d276646db7035a6c
Theo wald <theo83wald@gmail.com>
2021-05-27 14:31:30 +0200

    fin forms template

--- src/app/template/dinamicos/dinamicos.component.ts
+++ src/app/template/dinamicos/dinamicos.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+interface Persona {
+  nombre: string,
+  favoritos: Favorito[],
+}
+
+interface Favorito {
+  id: number;
+  nombre: string;
+}
+
 
 @Component({
   selector: 'app-dinamicos',
@@ -6,11 +18,40 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class DinamicosComponent implements OnInit {
+export class DinamicosComponent {
+
+  @ViewChild('formDinamico') formDinamico!: NgForm;
+
+  persona: Persona = {
+    nombre: 'Jean Leon',
+    favoritos: [
+      { id: 1, nombre: 'Need for Speed' },
+      { id: 2, nombre: 'Ratchet and Clank' }
+    ]
+  };
+
+  nuevoJuego: string = '';
 
   constructor() { }
 
-  ngOnInit(): void {
+
+  guardar(){
+    console.log("Guardar informacion");
+    console.log(this.formDinamico);
+  }
+
+  eliminar(index: number){
+    this.persona.favoritos.splice(index, 1);
+  }
+
+  agregar(){
+    // Su version
+    // const nuevoFavorito: Favorito = {
+    //   id: this.persona.favoritos.length + 1,
+    //   nombre: this.nuevoJuego
+    // };
+    // this.persona.favoritos.push(nuevoFavorito);
+    this.persona.favoritos.push({id: (this.persona.favoritos.length + 1), nombre: this.nuevoJuego });
   }
 
 }
--- src/app/template/switches/switches.component.ts
+++ src/app/template/switches/switches.component.ts
@@ -6,11 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SwitchesComponent implements OnInit {
+export class SwitchesComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  persona = {
+    genero: 'F',
+    notificaciones: true
   }
+
+  terminosCondiciones: boolean = false;
 
 }
